6e6ac9fee17b762c48ecb17911359ed95ccd5a2b
namitgpta <namitg677@gmail.com>
2020-10-29 19:06:53 +0530

    cinema dbms 29 Oct 2020_4

--- app/src/main/java/com/namit/cinemabookingsystem/review_payment.java
+++ app/src/main/java/com/namit/cinemabookingsystem/review_payment.java
@@ -2,7 +2,9 @@ package com.namit.cinemabookingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class review_payment extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class review_payment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_payment);
+
+        initial();
+
+    }
+
+    public void initial(){
+
+    }
+
+    public void to_payment_page_intent(View view) {
+        Intent intent=new Intent(review_payment.this, payment.class);
+        startActivity(intent);
     }
 }
