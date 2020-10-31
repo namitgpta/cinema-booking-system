a667246f31a6fe4ab8d90b7a4ba6f0609f33bd62
namitgpta <namitg677@gmail.com>
2020-11-01 01:09:22 +0530

    cinema dbms 31 Oct 2020_16

--- app/src/main/java/com/namit/cinemabookingsystem/after_payment.java
+++ app/src/main/java/com/namit/cinemabookingsystem/after_payment.java
@@ -1,14 +1,40 @@
 package com.namit.cinemabookingsystem;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class after_payment extends AppCompatActivity {
+    public SQLiteDatabase myDb;
+    public static Cursor c1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_payment);
+
+        initial();
+    }
+
+    private void initial(){
+        myDb=DatabaseHelper.myDataBase;
+        c1=myDb.rawQuery("select * from bookings", null);
+        c1.moveToFirst();
+    }
+
+    public void imageView_download(View view) {
+        printInvoice();
+    }
+
+    public void printInvoice(){
+        PdfDocument pdfDocument=new PdfDocument();
+        Paint myPaint=new Paint();
+
+
     }
 }
