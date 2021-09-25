d7c15f1ce378b04cf7bc5e02459e51e8d078c0aa
namitgpta <namitg677@gmail.com>
2021-09-25 07:53:06 +0530

    AWS Amplify DataStore NoSQL - Initial integration

---
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/UsersModel.java
@@ -0,0 +1,298 @@
+package com.amplifyframework.datastore.generated.model;
+
+import com.amplifyframework.core.model.temporal.Temporal;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Objects;
+
+import androidx.core.util.ObjectsCompat;
+
+import com.amplifyframework.core.model.AuthStrategy;
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.ModelOperation;
+import com.amplifyframework.core.model.annotations.AuthRule;
+import com.amplifyframework.core.model.annotations.Index;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+import com.amplifyframework.core.model.query.predicate.QueryField;
+
+import static com.amplifyframework.core.model.query.predicate.QueryField.field;
+
+/** This is an auto generated class representing the UsersModel type in your schema. */
+@SuppressWarnings("all")
+@ModelConfig(pluralName = "UsersModels", authRules = {
+  @AuthRule(allow = AuthStrategy.PUBLIC, operations = { ModelOperation.CREATE, ModelOperation.UPDATE, ModelOperation.DELETE, ModelOperation.READ })
+})
+public final class UsersModel implements Model {
+  public static final QueryField ID = field("UsersModel", "id");
+  public static final QueryField FIREBASE_ID = field("UsersModel", "firebaseId");
+  public static final QueryField LOCATION = field("UsersModel", "location");
+  public static final QueryField EMAIL = field("UsersModel", "email");
+  public static final QueryField NAME = field("UsersModel", "name");
+  public static final QueryField PHONE = field("UsersModel", "phone");
+  private final @ModelField(targetType="ID", isRequired = true) String id;
+  private final @ModelField(targetType="String", isRequired = true) String firebaseId;
+  private final @ModelField(targetType="String") String location;
+  private final @ModelField(targetType="String") String email;
+  private final @ModelField(targetType="String") String name;
+  private final @ModelField(targetType="String") String phone;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
+  public String getId() {
+      return id;
+  }
+  
+  public String getFirebaseId() {
+      return firebaseId;
+  }
+  
+  public String getLocation() {
+      return location;
+  }
+  
+  public String getEmail() {
+      return email;
+  }
+  
+  public String getName() {
+      return name;
+  }
+  
+  public String getPhone() {
+      return phone;
+  }
+  
+  public Temporal.DateTime getCreatedAt() {
+      return createdAt;
+  }
+  
+  public Temporal.DateTime getUpdatedAt() {
+      return updatedAt;
+  }
+  
+  private UsersModel(String id, String firebaseId, String location, String email, String name, String phone) {
+    this.id = id;
+    this.firebaseId = firebaseId;
+    this.location = location;
+    this.email = email;
+    this.name = name;
+    this.phone = phone;
+  }
+  
+  @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      } else if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      } else {
+      UsersModel usersModel = (UsersModel) obj;
+      return ObjectsCompat.equals(getId(), usersModel.getId()) &&
+              ObjectsCompat.equals(getFirebaseId(), usersModel.getFirebaseId()) &&
+              ObjectsCompat.equals(getLocation(), usersModel.getLocation()) &&
+              ObjectsCompat.equals(getEmail(), usersModel.getEmail()) &&
+              ObjectsCompat.equals(getName(), usersModel.getName()) &&
+              ObjectsCompat.equals(getPhone(), usersModel.getPhone()) &&
+              ObjectsCompat.equals(getCreatedAt(), usersModel.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), usersModel.getUpdatedAt());
+      }
+  }
+  
+  @Override
+   public int hashCode() {
+    return new StringBuilder()
+      .append(getId())
+      .append(getFirebaseId())
+      .append(getLocation())
+      .append(getEmail())
+      .append(getName())
+      .append(getPhone())
+      .append(getCreatedAt())
+      .append(getUpdatedAt())
+      .toString()
+      .hashCode();
+  }
+  
+  @Override
+   public String toString() {
+    return new StringBuilder()
+      .append("UsersModel {")
+      .append("id=" + String.valueOf(getId()) + ", ")
+      .append("firebaseId=" + String.valueOf(getFirebaseId()) + ", ")
+      .append("location=" + String.valueOf(getLocation()) + ", ")
+      .append("email=" + String.valueOf(getEmail()) + ", ")
+      .append("name=" + String.valueOf(getName()) + ", ")
+      .append("phone=" + String.valueOf(getPhone()) + ", ")
+      .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
+      .append("updatedAt=" + String.valueOf(getUpdatedAt()))
+      .append("}")
+      .toString();
+  }
+  
+  public static FirebaseIdStep builder() {
+      return new Builder();
+  }
+  
+  /** 
+   * WARNING: This method should not be used to build an instance of this object for a CREATE mutation.
+   * This is a convenience method to return an instance of the object with only its ID populated
+   * to be used in the context of a parameter in a delete mutation or referencing a foreign key
+   * in a relationship.
+   * @param id the id of the existing item this instance will represent
+   * @return an instance of this model with only ID populated
+   * @throws IllegalArgumentException Checks that ID is in the proper format
+   */
+  public static UsersModel justId(String id) {
+    try {
+      UUID.fromString(id); // Check that ID is in the UUID format - if not an exception is thrown
+    } catch (Exception exception) {
+      throw new IllegalArgumentException(
+              "Model IDs must be unique in the format of UUID. This method is for creating instances " +
+              "of an existing object with only its ID field for sending as a mutation parameter. When " +
+              "creating a new object, use the standard builder method and leave the ID field blank."
+      );
+    }
+    return new UsersModel(
+      id,
+      null,
+      null,
+      null,
+      null,
+      null
+    );
+  }
+  
+  public CopyOfBuilder copyOfBuilder() {
+    return new CopyOfBuilder(id,
+      firebaseId,
+      location,
+      email,
+      name,
+      phone);
+  }
+  public interface FirebaseIdStep {
+    BuildStep firebaseId(String firebaseId);
+  }
+  
+
+  public interface BuildStep {
+    UsersModel build();
+    BuildStep id(String id) throws IllegalArgumentException;
+    BuildStep location(String location);
+    BuildStep email(String email);
+    BuildStep name(String name);
+    BuildStep phone(String phone);
+  }
+  
+
+  public static class Builder implements FirebaseIdStep, BuildStep {
+    private String id;
+    private String firebaseId;
+    private String location;
+    private String email;
+    private String name;
+    private String phone;
+    @Override
+     public UsersModel build() {
+        String id = this.id != null ? this.id : UUID.randomUUID().toString();
+        
+        return new UsersModel(
+          id,
+          firebaseId,
+          location,
+          email,
+          name,
+          phone);
+    }
+    
+    @Override
+     public BuildStep firebaseId(String firebaseId) {
+        Objects.requireNonNull(firebaseId);
+        this.firebaseId = firebaseId;
+        return this;
+    }
+    
+    @Override
+     public BuildStep location(String location) {
+        this.location = location;
+        return this;
+    }
+    
+    @Override
+     public BuildStep email(String email) {
+        this.email = email;
+        return this;
+    }
+    
+    @Override
+     public BuildStep name(String name) {
+        this.name = name;
+        return this;
+    }
+    
+    @Override
+     public BuildStep phone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+    
+    /** 
+     * WARNING: Do not set ID when creating a new object. Leave this blank and one will be auto generated for you.
+     * This should only be set when referring to an already existing object.
+     * @param id id
+     * @return Current Builder instance, for fluent method chaining
+     * @throws IllegalArgumentException Checks that ID is in the proper format
+     */
+    public BuildStep id(String id) throws IllegalArgumentException {
+        this.id = id;
+        
+        try {
+            UUID.fromString(id); // Check that ID is in the UUID format - if not an exception is thrown
+        } catch (Exception exception) {
+          throw new IllegalArgumentException("Model IDs must be unique in the format of UUID.",
+                    exception);
+        }
+        
+        return this;
+    }
+  }
+  
+
+  public final class CopyOfBuilder extends Builder {
+    private CopyOfBuilder(String id, String firebaseId, String location, String email, String name, String phone) {
+      super.id(id);
+      super.firebaseId(firebaseId)
+        .location(location)
+        .email(email)
+        .name(name)
+        .phone(phone);
+    }
+    
+    @Override
+     public CopyOfBuilder firebaseId(String firebaseId) {
+      return (CopyOfBuilder) super.firebaseId(firebaseId);
+    }
+    
+    @Override
+     public CopyOfBuilder location(String location) {
+      return (CopyOfBuilder) super.location(location);
+    }
+    
+    @Override
+     public CopyOfBuilder email(String email) {
+      return (CopyOfBuilder) super.email(email);
+    }
+    
+    @Override
+     public CopyOfBuilder name(String name) {
+      return (CopyOfBuilder) super.name(name);
+    }
+    
+    @Override
+     public CopyOfBuilder phone(String phone) {
+      return (CopyOfBuilder) super.phone(phone);
+    }
+  }
+  
+}
--- app/src/main/java/com/namit/cinemabookingsystem/InsideBody.java
+++ app/src/main/java/com/namit/cinemabookingsystem/InsideBody.java
@@ -17,6 +17,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.core.model.query.Where;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.generated.model.UsersModel;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,7 +35,7 @@ import java.io.IOException;
 public class InsideBody extends AppCompatActivity {
 
     private FirebaseUser firebaseUser;
-    public static String tagCinema="tagCinema";
+    public static String tagCinema = "tagCinema";
     DatabaseHelper myDbHelper;
     public SQLiteDatabase myDb;
     private StorageReference mStorageRef;
@@ -40,9 +46,10 @@ public class InsideBody extends AppCompatActivity {
     private ImageButton nextButton, prevButton;
     private ImageView movies_imageView;
     private TextView counter_textView;
-    public static int current_counter=1;
+    public static int current_counter = 1;
     private String[] movies_list, movies_title_list, release_date_list, movieDuration_list, rating_list;
     private TextView film_heading_textView, release_date_textView, filmDuration_textView, filmRating_textView;
+    private TextView greetingTextView;
 
     @SuppressLint("ResourceType")
     @Override
@@ -75,117 +82,153 @@ public class InsideBody extends AppCompatActivity {
             myDb = myDbHelper.myDataBase;
         } // firebase user and Database connection details
 
-        mStorageRef= FirebaseStorage.getInstance().getReference();
+        // AWS starts
+        try {
+            Amplify.addPlugin(new AWSApiPlugin()); // UNCOMMENT this line once backend is deployed
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i("Amplify", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("Amplify", "Could not initialize Amplify", error);
+        }
+        // AWS ends
+
+        greetingTextView = findViewById(R.id.greeting);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         initial();
 
-        nextButton=findViewById(R.id.next_imageButton);
-        prevButton=findViewById(R.id.prev_imageButton);
-        movies_imageView=findViewById(R.id.imageView_movies);
-        counter_textView=findViewById(R.id.counter_movies);
-        film_heading_textView=findViewById(R.id.film_name);
-        release_date_textView=findViewById(R.id.release_date_textview);
-        filmDuration_textView=findViewById(R.id.duration_textview);
-        filmRating_textView=findViewById(R.id.rating_textview);
+        nextButton = findViewById(R.id.next_imageButton);
+        prevButton = findViewById(R.id.prev_imageButton);
+        movies_imageView = findViewById(R.id.imageView_movies);
+        counter_textView = findViewById(R.id.counter_movies);
+        film_heading_textView = findViewById(R.id.film_name);
+        release_date_textView = findViewById(R.id.release_date_textview);
+        filmDuration_textView = findViewById(R.id.duration_textview);
+        filmRating_textView = findViewById(R.id.rating_textview);
 
-        try(Cursor c1=myDb.rawQuery("select distinct name from films where _id in (select film_id from screenings where _id in (select screening_id from movies_ads))", null);
-            Cursor c2=myDb.rawQuery("select * from films", null)){
-            String counter_initial_text=current_counter+" out of "+c1.getCount();
+        try (Cursor c1 = myDb.rawQuery("select distinct name from films where _id in (select film_id from screenings where _id in (select screening_id from movies_ads))", null);
+             Cursor c2 = myDb.rawQuery("select * from films", null)) {
+            String counter_initial_text = current_counter + " out of " + c1.getCount();
             counter_textView.setText(counter_initial_text);
-            movies_list=new String[c1.getCount()];
-            movies_title_list=new String[c1.getCount()];
-            release_date_list=new String[c1.getCount()];
-            movieDuration_list=new String[c1.getCount()];
-            rating_list=new String[c1.getCount()];
-            int i=0;
-            while (c1.moveToNext()){
-                movies_list[i++]=c1.getString(0);
+            movies_list = new String[c1.getCount()];
+            movies_title_list = new String[c1.getCount()];
+            release_date_list = new String[c1.getCount()];
+            movieDuration_list = new String[c1.getCount()];
+            rating_list = new String[c1.getCount()];
+            int i = 0;
+            while (c1.moveToNext()) {
+                movies_list[i++] = c1.getString(0);
             }
-            int resId=getResources().getIdentifier(movies_list[0], "raw", getPackageName());
+            int resId = getResources().getIdentifier(movies_list[0], "raw", getPackageName());
             movies_imageView.setBackgroundResource(resId);
 
-            i=0;
-            while (c2.moveToNext()){
-                movies_title_list[i]=c2.getString(3);
-                release_date_list[i]=c2.getString(4);
-                movieDuration_list[i]=c2.getString(2);
-                rating_list[i++]=c2.getString(5);
+            i = 0;
+            while (c2.moveToNext()) {
+                movies_title_list[i] = c2.getString(3);
+                release_date_list[i] = c2.getString(4);
+                movieDuration_list[i] = c2.getString(2);
+                rating_list[i++] = c2.getString(5);
             }
             set_movie_details(0);
         }
 
         nextButton.setOnClickListener(v -> {
-            current_counter=current_counter==movies_list.length?movies_list.length:current_counter+1;
-            String ctr_text=current_counter+" out of "+movies_list.length;
+            current_counter = current_counter == movies_list.length ? movies_list.length : current_counter + 1;
+            String ctr_text = current_counter + " out of " + movies_list.length;
             counter_textView.setText(ctr_text);
-            int resId=getResources().getIdentifier(movies_list[current_counter-1], "raw", getPackageName());
+            int resId = getResources().getIdentifier(movies_list[current_counter - 1], "raw", getPackageName());
             movies_imageView.setBackgroundResource(resId);
 
-            set_movie_details(current_counter-1);
+            set_movie_details(current_counter - 1);
         });
         prevButton.setOnClickListener(v -> {
-            current_counter=current_counter==1?1:current_counter-1;
-            String ctr_text=current_counter+" out of "+movies_list.length;
+            current_counter = current_counter == 1 ? 1 : current_counter - 1;
+            String ctr_text = current_counter + " out of " + movies_list.length;
             counter_textView.setText(ctr_text);
-            int resId=getResources().getIdentifier(movies_list[current_counter-1], "raw", getPackageName());
+            int resId = getResources().getIdentifier(movies_list[current_counter - 1], "raw", getPackageName());
             movies_imageView.setBackgroundResource(resId);
 
-            set_movie_details(current_counter-1);
+            set_movie_details(current_counter - 1);
         });
 
 
     }
 
-    private void initial(){
+    private void initial() {
         myDb.execSQL("update customers set _id=?", new String[]{firebaseUser.getUid()});
         myDb.execSQL("update customers set email=?", new String[]{firebaseUser.getEmail()});
-        String name=getIntent().getStringExtra("name");
-        String phone=getIntent().getStringExtra("phone");
-        if(name!=null && phone!=null){
+        String name = getIntent().getStringExtra("name");
+        String phone = getIntent().getStringExtra("phone");
+        if (name != null && phone != null) {
             myDb.execSQL("update customers set name=?", new String[]{name});
             myDb.execSQL("update customers set phone=?", new String[]{phone});
         }
+
+        //AWS Amplify
+        //User details checking
+        String firebaseUid = firebaseUser.getUid();
+
+        Amplify.DataStore.query(
+                UsersModel.class, Where.matches(UsersModel.FIREBASE_ID.eq(firebaseUid)),
+                items -> {
+                    String awsEmail = null;
+                    String awsName = null;
+
+                    while (items.hasNext()) {
+                        UsersModel item = items.next();
+                        Log.i("Amplify", "Id " + item.getId());
+                        awsEmail = item.getEmail();
+                        awsName = item.getName();
+                    }
+                    //System.out.println(awsEmail);
+                    if (awsEmail == null) {
+                        // Add to AWS Amplify DataStore
+                        UsersModel item = UsersModel.builder()
+                                .firebaseId(firebaseUid)
+                                .location("")
+                                .email(firebaseUser.getEmail())
+                                .name(getIntent().getStringExtra("name"))
+                                .phone(getIntent().getStringExtra("phone"))
+                                .build();
+                        Amplify.DataStore.save(
+                                item,
+                                success -> Log.i("Amplify", "Saved item: " + success.item().getId()),
+                                error -> Log.e("Amplify", "Could not save item to DataStore", error)
+                        );
+                        awsEmail = firebaseUser.getEmail();
+                        awsName = getIntent().getStringExtra("name");
+                    }
+                    String greet = "Hello, " + awsName + " (" + awsEmail + ")";
+                    greetingTextView.setText(greet);
+                },
+                failure -> Log.e("Amplify", "Could not query DataStore", failure)
+        );
+
     }
 
     public void movie_imageView_external_click(View view) {
-        try(Cursor c1=myDb.rawQuery("select link from imdb_links where film_id=?", new String[]{String.valueOf(current_counter)})){
+        try (Cursor c1 = myDb.rawQuery("select link from imdb_links where film_id=?", new String[]{String.valueOf(current_counter)})) {
             c1.moveToFirst();
-            Intent external_link=new Intent(Intent.ACTION_VIEW, Uri.parse(c1.getString(0)));
+            Intent external_link = new Intent(Intent.ACTION_VIEW, Uri.parse(c1.getString(0)));
             startActivity(external_link);
         }
     }
 
-    private void set_movie_details(int index){
+    private void set_movie_details(int index) {
         film_heading_textView.setText(movies_title_list[index]);
-        String release_date="Release Date: "+release_date_list[index];
+        String release_date = "Release Date: " + release_date_list[index];
         release_date_textView.setText(release_date);
-        String duration="Duration: "+movieDuration_list[index]+" hrs";
+        String duration = "Duration: " + movieDuration_list[index] + " hrs";
         filmDuration_textView.setText(duration);
-        String rating="Rating: "+rating_list[index];
+        String rating = "Rating: " + rating_list[index];
         filmRating_textView.setText(rating);
     }
 
     public void book_layout_intent_change(View view) {
-        Intent intent=new Intent(InsideBody.this, book.class);
+        Intent intent = new Intent(InsideBody.this, book.class);
         startActivity(intent);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     @Override
@@ -194,7 +237,7 @@ public class InsideBody extends AppCompatActivity {
         super.onStop();
     }
 
-    public void upload(){
+    public void upload() {
         riversRef = mStorageRef.child("DBs/cinema.db");
         riversRef.putFile(file)
                 .addOnSuccessListener(taskSnapshot -> {
@@ -209,7 +252,7 @@ public class InsideBody extends AppCompatActivity {
     }
 
     public void download(View view) {
-        riversRef=mStorageRef.child("DBs/cinema.db");
+        riversRef = mStorageRef.child("DBs/cinema.db");
         riversRef.getDownloadUrl().addOnSuccessListener(uri -> {
             downloadFile(InsideBody.this, "cinema", ".db", "Download/Cinema_temp", uri.toString());
             Log.d(tagCinema, "download file success");
@@ -217,12 +260,12 @@ public class InsideBody extends AppCompatActivity {
     }
 
     private void downloadFile(Context context, String filename, String fileExtension, String destinationDirectory, String url) {
-        DownloadManager downloadManager=(DownloadManager)context.getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri uri=Uri.parse(url);
-        DownloadManager.Request request=new DownloadManager.Request(uri);
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
 
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalFilesDir(context, destinationDirectory, filename+fileExtension);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, filename + fileExtension);
 
         downloadManager.enqueue(request);
     }
